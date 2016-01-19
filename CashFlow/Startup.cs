3f44a0ae3273733d03a6ba3c98cd51620330b95e
Aquarelius <aq19822@hotmail.com>
2016-01-19 17:07:22 +0600

    Project's base

---
+++ CashFlow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CashFlow.Startup))]
+namespace CashFlow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
